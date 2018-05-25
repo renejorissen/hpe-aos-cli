064c5483eaa46658474fdeb57d6ae085b40f4410
renejorissen <rene@booches.nl>
2018-05-25 17:43:27 +0200

    only authentication to ArubaOS switches

---
+++ auth.py
@@ -0,0 +1,54 @@
+##############################################################
+##
+##
+## DEFAULT LOGIN & LOGOUT SCRIPT
+##
+##
+## USEFULL URL: https://community.arubanetworks.com/t5/Forum-Fran%C3%A7ais/How-to-REST-API-sur-ArubaOS-Switch/m-p/305020
+##############################################################
+import requests
+import urllib3
+import getpass
+import json
+import base64
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+##############################################################
+## GET BASE INFORMATION
+##############################################################
+username = input('Enter username: ')
+password = input('Enter password: ')
+credentials = {
+               "userName" : username,
+               "password" : password
+                }
+
+ip_address = "10.10.1.3"
+url = 'https://{}/rest/v3/'.format(ip_address)
+
+
+##############################################################
+## LOGIN AND GET THE COOKIE
+##############################################################
+get_cookie = requests.post(url + 'login-sessions', data=json.dumps(credentials), verify=False, timeout=2)
+
+print("Login Status", get_cookie.status_code)
+if get_cookie.status_code == 201:
+    print("LOGIN SUCCESS!")
+else:
+    print("SOMETHING WENT WRONG!")
+
+cookie = get_cookie.json()['cookie']
+headers = {"Cookie" : cookie}
+
+
+##############################################################
+## LOGOUT AND GOODBYE
+##############################################################
+delete_session = requests.delete(url + 'login-sessions', headers=headers, verify=False, timeout=2)
+print("Logout Status: {}".format(delete_session.status_code))
+if delete_session.status_code == 204:
+    print("LOGOUT SUCCESS - GOODBYE!!!")
+else:
+    print("LOGOUT WASN'T SUCCESSFUL")
